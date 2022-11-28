c82b184bab9099a7523fb6f481c8c6789370554d
Vadim Igdisanov <Igidsanov@t-dev.pro>
2022-11-28 16:33:51 +0500

    Initial Commit

---
+++ ImageForSfera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageForSfera
+//
+//  Created by Vadim Igdisanov on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
