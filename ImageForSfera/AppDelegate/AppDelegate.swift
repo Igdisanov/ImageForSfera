d057967dbdc2e6931aa1bc7df8fcee6414865406
Vadim Igdisanov <Igidsanov@t-dev.pro>
2022-12-02 15:34:50 +0500

    Start project

--- ImageForSfera/AppDelegate/AppDelegate.swift
+++ ImageForSfera/AppDelegate/AppDelegate.swift
@@ -7,14 +7,17 @@
 
 import UIKit
 import CoreData
+import TabBarController
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
         return true
     }
 
